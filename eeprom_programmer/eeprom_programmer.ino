f56f7c2132f26ddbe041449499a76997603224ed
Tyler <ty.jbailey32@gmail.com>
2021-05-08 16:03:34 -0700

    Writes & reads successfully now.

--- eeprom_programmer/eeprom_programmer.ino
+++ eeprom_programmer/eeprom_programmer.ino
@@ -17,23 +17,24 @@ void latchReg() {
 void writeMem(uint16_t addr, uint8_t data) {
     digitalWrite(TRI_EN, HIGH);
     digitalWrite(MEM_OE_, HIGH); // 15ns setup time and 15ns hold time covered by 1 clock cycle delay (62.5ns)
-    /* [A0 ... A10] [IO0 ... IO7] */
+    
+    /* [UNUSED0 ... UNUSED4] [A0 ... A10] [IO0 ... IO7] */
+    digitalWrite(REG_OE_, HIGH); // Disable shift register output
     shiftOut(DATA, SER_CLK, MSBFIRST, data);
     latchReg();
     shiftOut(DATA, SER_CLK, MSBFIRST, addr);
     latchReg();
-    // Write the last 3 bits of the address
-    for (int i = 0; i < 3; i++) {
-        digitalWrite(DATA, !!((addr >> 8) >> i));
-        digitalWrite(SER_CLK, HIGH);
-        digitalWrite(SER_CLK, LOW);
-    }
+    shiftOut(DATA, SER_CLK, MSBFIRST, (addr >> 8));
     latchReg();
+    digitalWrite(REG_OE_, LOW); // Enable shift register output
+    delayMicroseconds(1);
     
     // Write cycle using the WE' controlled case
     digitalWrite(MEM_WE_, LOW); // Data latched
     delayMicroseconds(1); // Taking t_wp as 1us since its greater than 150ns and has no max
     digitalWrite(MEM_WE_, HIGH); // Address latched
+    delayMicroseconds(1);
+    digitalWrite(MEM_WE_, LOW);
     delayMicroseconds(1);
 }
 
@@ -42,17 +43,17 @@ void writeMem(uint16_t addr, uint8_t data) {
 void readMem(uint16_t addr) {
     digitalWrite(TRI_EN, LOW); // Tri-state buffer output set to hi-Z
     digitalWrite(MEM_WE_, HIGH);
+
+    digitalWrite(REG_OE_, HIGH); // Disable shift register output
     shiftOut(DATA, SER_CLK, MSBFIRST, 0x00);
     latchReg();
     shiftOut(DATA, SER_CLK, MSBFIRST, addr);
     latchReg();
-    // Write the last 3 bits of the address
-    for (int i = 0; i < 3; i++) {
-        digitalWrite(DATA, !!((addr >> 8) >> i));
-        digitalWrite(SER_CLK, HIGH);
-        digitalWrite(SER_CLK, LOW);
-    }
+    shiftOut(DATA, SER_CLK, MSBFIRST, (addr >> 8));
     latchReg();
+    digitalWrite(REG_OE_, LOW); // Enable shift register output
+    delayMicroseconds(1);
+    
     digitalWrite(MEM_OE_, LOW);
     delayMicroseconds(1);
 }
@@ -66,40 +67,15 @@ void setup() {
     pinMode(MEM_WE_, OUTPUT);
     pinMode(TRI_EN, OUTPUT);
 
-    digitalWrite(TRI_EN, HIGH);
-    digitalWrite(MEM_WE_, HIGH);
-    digitalWrite(REG_OE_, LOW);
 
-//    writeMem(0, 0x00);
+//    writeMem(1, 0x5);
 //    delay(5000);
-//    readMem(0);
-//    delay(5000);
-      digitalWrite(DATA, HIGH);
-      digitalWrite(SER_CLK, HIGH);
-      digitalWrite(SER_CLK, LOW);
-      delay(10);
-}
-
-void updateReg() {
-    digitalWrite(REG_CLK, LOW);
-    
+    readMem(0);
+    delay(5000);
+    readMem(1);
+    delay(5000);
 }
 
 void loop() {
-    latchReg();
-//    shiftOut(DATA, SER_CLK, MSBFIRST, 0x5);
-//    latchReg();
-//    delay(500);
-//    shiftOut(DATA, SER_CLK, MSBFIRST, 0x00);
-//    latchReg();
-//    delay(500);
-//    for (int i = 0; i < 3; i++) {
-//        digitalWrite(DATA, HIGH);
-//        digitalWrite(SER_CLK, HIGH);
-//        digitalWrite(SER_CLK, LOW);
-//    }
-//    latchReg();
-    delay(200);
-    
-//    delay(500);
+
 }
