64fbd05e7ff16ddf75ae1947f00ec0df540d2c3b
Tyler <ty.jbailey32@gmail.com>
2021-05-08 22:40:26 -0700

    Implemented read function to parse sentences to tokenize user input.

--- eeprom_interface/SerialInterface.c
+++ eeprom_interface/SerialInterface.c
@@ -2,19 +2,59 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdbool.h>
+#include <errno.h>
+#include <string.h>
 
 #define _VERSION 0
-#define COMMAND_LENGTH 1024
+#define MAX_COMMAND_LENGTH 256
+#define MAX_ARG_COUNT 10
 
-bool running = true;
-
-void read_command(char *buffer, char *args);
+int read_command(char *buffer, char *argv[]);
 
 int main(int argc, char *argv[]) {
-    printf("EEPROM Serial Interface V%d", _VERSION);
-    while (running) {
-        
-    }
+    char input_buffer[MAX_COMMAND_LENGTH];
+    char *args[MAX_ARG_COUNT];
 
-    return 0;
+    printf("EEPROM Serial Interface V%d\n", _VERSION);
+    while (true) {
+        int len = read_command(input_buffer, args);
+        printf("COUNT: %d\n", len);
+        for (int i = 0; i < len; i++) {
+            printf("ARG%d: %s\n", i, args[i]);
+        }
+        printf("End of args\n");
+    }
+    
+    return EXIT_SUCCESS;
+}
+
+/*
+ * Waits for keyboard input and places the received string into "buffer" and parses the arguments from that string and places them in "args".
+ * Returns the number of arguments parsed
+ */
+int read_command(char *buffer, char *args[]) {
+    // Read input & get length
+    int length = read(STDIN_FILENO, buffer, MAX_COMMAND_LENGTH - 1);
+    if (length < 0 && errno != EINTR) {
+        perror("Unable to read input.\n");
+        exit(EXIT_FAILURE);
+    }
+    // Null terminate the buffer so it can be interpretted as a string
+    buffer[length - 1] = '\0';
+    // Parse tokens from the buffer
+    int arg_count = 0;
+    bool delimiter = true;
+    for (int i = 0; i < length - 1; i++) {
+        if (buffer[i] == ' ' || buffer[i] == '\t' || buffer[i] == '\n') {
+            // If current char is a delimiter, null terminate to indicate end of argument
+            delimiter = true;
+            buffer[i] = '\0';
+
+        } else if (delimiter) {
+            args[arg_count] = &buffer[i];
+            arg_count++;
+            delimiter = false;
+        }
+    }
+    return arg_count;
 }
